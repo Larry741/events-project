21220aa8b7dc817997afc336bb766eea42979580
Larry741 <85595046+Larry741@users.noreply.github.com>
2022-06-23 15:35:40 +0100

    edit pages

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
+  return (
+    <div>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    },
+    revalidate: 1800
+  }
+}
+
+export default HomePage;
